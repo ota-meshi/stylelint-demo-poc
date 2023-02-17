5c3a6728ba766e0c27793871ee0db1552d274c4f
yosuke ota <otameshiyo23@gmail.com>
2023-02-17 10:44:47 +0900

    use ansi-regex

--- src/notification/index.ts
+++ src/notification/index.ts
@@ -1,7 +1,10 @@
+import ansiRegex from "ansi-regex";
+const ansiRe = ansiRegex();
 export type NotificationPanel = {
   hide(): unknown;
   append(string: string): unknown;
 };
+const CHA = "\u001b[1G";
 export function setupNotificationPanel({
   rootElement,
 }: {
@@ -9,7 +12,22 @@ export function setupNotificationPanel({
 }): NotificationPanel {
   return {
     append: (string: string) => {
-      rootElement.textContent += string;
+      let start = 0;
+
+      for (const match of string.matchAll(ansiRe)) {
+        if (match[0] === CHA) {
+          const lastLinefeed = rootElement.textContent!.lastIndexOf("\n");
+          if (lastLinefeed > -1)
+            rootElement.textContent = rootElement.textContent!.slice(
+              0,
+              lastLinefeed + 1
+            );
+        }
+        console.log(match[0]);
+        rootElement.textContent += string.slice(start, match.index!);
+        start = match.index! + match[0].length;
+      }
+      rootElement.textContent += string.slice(start);
     },
     hide() {
       rootElement.style.display = "none";
